66a7516c30b03dc54f1c8c70f5e62a9762121455
Javidan Taghiyev <jtaghiyev16178@ada.edu.az>
2026-01-11 17:44:16 +0400

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pd
-import string
 
 def from_numpy(array):
-    columns = list(string.ascii_uppercase[:array.shape[1]])
+    columns = [chr(ord('A') + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=columns)
