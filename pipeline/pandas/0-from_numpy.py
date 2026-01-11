0c18d1f03b03a27ea4b1f2efe6a0ea6c7b1a804c
Javidan Taghiyev <jtaghiyev16178@ada.edu.az>
2026-01-11 17:46:34 +0400

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pandas as pd
 
+
 def from_numpy(array):
     columns = [chr(ord('A') + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=columns)
