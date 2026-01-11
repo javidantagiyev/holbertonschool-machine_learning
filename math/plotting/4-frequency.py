0233605614ecc6a1f744a7046479cce8d9347429
Javidan Taghiyev <jtaghiyev16178@ada.edu.az>
2026-01-11 22:13:24 +0400

    Task all

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,24 +11,18 @@ import matplotlib.pyplot as plt
 
 def frequency():
     """
-    Plot a histogram of student scores with bins every 10 units.
+    Plot a histogram of student scores.
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
     plt.figure(figsize=(6.4, 4.8))
 
-    # Histogram: bins every 10 units, outlined in black
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, range=(0, 100), edgecolor='black')
+    # EXACT reference behavior
+    plt.hist(student_grades, bins=10, edgecolor='black')
 
-    # Labels and title
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-
-    # Match reference axis formatting
-    plt.xlim(0, 100)
-    plt.xticks(np.arange(0, 101, 10))
 
     plt.show()
