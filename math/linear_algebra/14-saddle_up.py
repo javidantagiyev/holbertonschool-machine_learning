e6953b15adda5a1b676dfde4b70c26c46cb9b559
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:54:04 +0400

    Update 14-saddle_up.py

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+Performs matrix multiplication using NumPy.
+"""
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication.
+
+    Args:
+        mat1 (numpy.ndarray): First matrix.
+        mat2 (numpy.ndarray): Second matrix.
+
+    Returns:
+        numpy.ndarray: Product of the matrices.
+    """
     return np.matmul(mat1, mat2)
