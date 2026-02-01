4ada5155e25c06b0904255e9321b20032a7ec7db
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:48:27 +0400

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
+"""
+Transposes a 2D matrix.
+"""
 
 
 def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+
+    Args:
+        matrix (list of lists): Input matrix.
+
+    Returns:
+        list of lists: Transposed matrix.
+    """
+    # Swap rows and columns using list comprehension
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
