719f524afe1b9c0069cb0c77e0b1325e46de7a5f
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:52:32 +0400

    Update 11-the_western_exchange.py

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+"""
+Transposes a NumPy array.
+"""
 
 
 def np_transpose(matrix):
+    """
+    Returns the transpose of a NumPy array.
+
+    Args:
+        matrix (numpy.ndarray): Input array.
+
+    Returns:
+        numpy.ndarray: Transposed array.
+    """
     return matrix.T
