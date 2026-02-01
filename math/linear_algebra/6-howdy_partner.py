3ea5aca84549010bf929de27a0397572cda606db
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:49:48 +0400

    Update 6-howdy_partner.py

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
+"""
+Concatenates two arrays.
+"""
 
 
 def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Args:
+        arr1 (list): First array.
+        arr2 (list): Second array.
+
+    Returns:
+        list: New concatenated array.
+    """
     return arr1 + arr2
