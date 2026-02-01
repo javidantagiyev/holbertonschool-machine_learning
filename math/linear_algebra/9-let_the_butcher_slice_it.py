e261d4f0cf3cb01e63a9b23d137612900d0c4569
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:59:15 +0400

    Update 9-let_the_butcher_slice_it.py

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-"""
-Demonstrates slicing on NumPy arrays.
-"""
 import numpy as np
 
 matrix = np.array([
@@ -11,13 +8,8 @@ matrix = np.array([
     [19, 20, 21, 22, 23, 24]
 ])
 
-# Middle two rows
 mat1 = matrix[1:3, :]
-
-# Middle two columns
 mat2 = matrix[:, 2:4]
-
-# Bottom-right 3x3 submatrix
 mat3 = matrix[1:4, 3:6]
 
 print("The middle two rows of the matrix are:\n{}".format(mat1))
