2b78d57ed31f000e6875ac2fc2c996aef911cd43
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:51:23 +0400

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
+"""
+Performs matrix multiplication.
+"""
 
 
 def mat_mul(mat1, mat2):
+    """
+    Multiplies two matrices.
+
+    Args:
+        mat1 (list of lists): First matrix.
+        mat2 (list of lists): Second matrix.
+
+    Returns:
+        list of lists or None: Product matrix or None if invalid.
+    """
+    # Number of columns in mat1 must equal rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
 
     result = []
+
     for row in mat1:
         new_row = []
         for j in range(len(mat2[0])):
+            # Dot product
             new_row.append(sum(row[i] * mat2[i][j] for i in range(len(mat2))))
         result.append(new_row)
+
     return result
