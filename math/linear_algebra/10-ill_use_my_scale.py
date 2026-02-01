02b633f5e3d8faf4dec2afe5cc9bb721d7cecfb9
Javidan Taghiyev <118770534+javidantagiyev@users.noreply.github.com>
2026-02-01 21:52:10 +0400

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+"""
+Returns the shape of a NumPy array.
+"""
 
 
 def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray.
+
+    Args:
+        matrix (numpy.ndarray): Input array.
+
+    Returns:
+        tuple: Shape of the array.
+    """
     return matrix.shape
