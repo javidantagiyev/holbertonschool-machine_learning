fe0f928bcd82f492b4aab54b45110fd5a77a4c77
Javidan Taghiyev <jtaghiyev16178@ada.edu.az>
2026-02-22 21:12:32 +0400

    init

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,12 @@
+def max_depth_below(self):
+    """
+    Returns the maximum depth below this node (including leaves)
+    """
+    # If node is a leaf (safety check)
+    if self.is_leaf:
+        return self.depth
+
+    left_depth = self.left_child.max_depth_below() if self.left_child else self.depth
+    right_depth = self.right_child.max_depth_below() if self.right_child else self.depth
+
+    return max(left_depth, right_depth)
